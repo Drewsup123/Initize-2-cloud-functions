de6797bc465a101234a2bee1cdb6b50cf72f93e3
Drew <drewsup123c@gmail.com>
2019-09-06 15:19:49 -0600

    setup firebase functions and added deleteExpiredInvites

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.deleteExpiredInvites = functions.database.ref('/invites')
+    .onWrite(change => {
+        const ref = change.after.ref;
+        // This sets the cutoff date to two days ago
+        const cutoffDate = Date.now() - (2 * 60 * 60 * 24 * 1000);
+        // returns invites that are older than 2 days old
+        let oldInvites = ref.orderByChild('createdAt').endAt(cutoffDate);
+        // updates the database to delete all invites that are invalid
+        return oldInvites.once('value', snap => {
+            const updates = {};
+            snap.forEach(child => {
+                updates[child.key] = null
+            })
+            return ref.update(updates);
+        })
+    })
